84815e729a15380f87a047c9ba6bb64e5d9c427c
ZeroNMT <minhtham0zero@gmail.com>
2020-02-29 17:47:25 +0700

    [FIX] Update comment for code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 from numpy import inf
 
-# given values for the problems
-
+### given values for the problems
 dist_matrix = np.array([
     [0, 10, 12, 11, 14, 16, 17, 17, 20, 25],
     [10, 0, 13, 15, 8, 23, 22, 27, 13, 15],
@@ -33,108 +32,72 @@ fee_matrix = np.array([
 iteration = 100
 n_ants = 10
 n_citys = 10
+n_choose_point = 4
+n_orders = 5
 
-# intialization part
-
+### intialization part
 m = n_ants
 n = n_citys
 e = .5  # evaporation rate
 alpha = 1  # pheromone factor
 beta = 2  # visibility factor
 
-# calculating the visibility of the next city visibility(i,j)=1/d(i,j)
-
+### calculating the visibility of the next city visibility(i,j)=1/d(i,j)
 visibility = 1/dist_matrix
 visibility[visibility == inf] = 0
 
-# intializing pheromne present at the paths to the cities
-
-pheromne = .1*np.ones((m, n))
-
 # intializing the rute of the ants with size rute(n_ants,n_citys+1)
 # note adding 1 because we want to come back to the source city
-
 rute = np.ones((m, n+1))
 
-for ite in range(iteration):
+pheromne = .1*np.ones((m, n))  #intializing pheromne present at the paths to the cities
 
-    # initial starting and ending positon of every ants '1' i.e city '1'
-    rute[:, 0] = 1
+for ite in range(iteration):
+    rute[:, 0] = 1 # initial starting and ending positon of every ants '1' i.e city '1'
 
     for i in range(m):
-
         temp_visibility = np.array(visibility)  # creating a copy of visibility
 
-        for j in range(n-1):
-            # print(rute)
-
-            # intializing combine_feature array to zero
-            combine_feature = np.zeros(n)
-            # intializing cummulative probability array to zeros
-            cum_prob = np.zeros(n)
+        for j in range(n-2): # expect initial point and last point
+            combine_feature = np.zeros(n) # intializing combine_feature array to zero            
+            cum_prob = np.zeros(n) # intializing cummulative probability array to zeros
 
             cur_loc = int(rute[i, j]-1)  # current city of the ant
+            temp_visibility[:, cur_loc] = 0 # making visibility of the current city as zero
 
-            # making visibility of the current city as zero
-            temp_visibility[:, cur_loc] = 0
+            p_feature = np.power(pheromne[cur_loc, :], beta) # calculating pheromne feature
+            p_feature = p_feature[:, np.newaxis] # adding axis to make a size[5,1]            
+            v_feature = np.power(temp_visibility[cur_loc, :], alpha) # calculating visibility feature
+            v_feature = v_feature[:, np.newaxis] # adding axis to make a size[5,1]
 
-            # calculating pheromne feature
-            p_feature = np.power(pheromne[cur_loc, :], beta)
-            # calculating visibility feature
-            v_feature = np.power(temp_visibility[cur_loc, :], alpha)
-
-            # adding axis to make a size[5,1]
-            p_feature = p_feature[:, np.newaxis]
-            # adding axis to make a size[5,1]
-            v_feature = v_feature[:, np.newaxis]
-
-            # calculating the combine feature
-            combine_feature = np.multiply(p_feature, v_feature)
-
+            combine_feature = np.multiply(p_feature, v_feature) # calculating the combine feature
             total = np.sum(combine_feature)  # sum of all the feature
-
-            # finding probability of element probs(i) = comine_feature(i)/total
-            probs = combine_feature/total
-
+           
+            probs = combine_feature/total # finding probability of element probs(i) = comine_feature(i)/total
             cum_prob = np.cumsum(probs)  # calculating cummulative sum
-            # print(cum_prob)
-            r = np.random.random_sample()  # randon no in [0,1)
-            # print(r)
-            # finding the next city having probability higher then random(r)
-            city = np.nonzero(cum_prob > r)[0][0]+1
-            # print(city)
 
+            r = np.random.random_sample()  # random no in [0,1)
+            city = np.nonzero(cum_prob > r)[0][0]+1 # finding the next city having probability higher then random(r)
             rute[i, j+1] = city  # adding city to route
 
-        # finding the last untraversed city to route
-        left = list(set([i for i in range(1, n+1)])-set(rute[i, :-2]))[0]
+        last_point = list(set([i for i in range(1, n+1)])-set(rute[i, :-2]))[0] # finding the last untraversed city to route
+        rute[i, -2] = last_point  # adding untraversed city to route
 
-        rute[i, -2] = left  # adding untraversed city to route
-
+    ### Calcualting total distance for each route
     rute_opt = np.array(rute)  # intializing optimal route
-
-    # intializing total_distance_of_tour with zero
-    dist_cost = np.zeros((m, 1))
-
+    dist_cost = np.zeros((m, 1)) # intializing total_distance_of_tour with zero
     for i in range(m):
-
         s = 0
         for j in range(n-1):
+            s = s + dist_matrix[int(rute_opt[i, j])-1, int(rute_opt[i, j+1])-1] # calcualting total tour distance
+        dist_cost[i] = s # storing distance of tour for 'i'th ant at location 'i'
 
-            # calcualting total tour distance
-            s = s + dist_matrix[int(rute_opt[i, j])-1, int(rute_opt[i, j+1])-1]
-
-        # storing distance of tour for 'i'th ant at location 'i'
-        dist_cost[i] = s
-
-    # finding location of minimum of dist_cost
-    dist_min_loc = np.argmin(dist_cost)
+    dist_min_loc = np.argmin(dist_cost) # finding location of minimum of dist_cost
     dist_min_cost = dist_cost[dist_min_loc]  # finging min of dist_cost
+    best_route = rute[dist_min_loc, :]  # intializing current traversed as best route
 
-    # intializing current traversed as best route
-    best_route = rute[dist_min_loc, :]
+    ### Updating the pheromne
     pheromne = (1-e)*pheromne  # evaporation of pheromne with (1-e)
-
     for i in range(m):
         for j in range(n-1):
             dt = 1/dist_cost[i]
